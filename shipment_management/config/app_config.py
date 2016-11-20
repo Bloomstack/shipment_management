1b2ead776b736744765addf8bb3c04e9ce2ef6eb
KaterinaR <romanchuk.katerina@gmail.com>
2016-11-20 15:23:58 +0200

    Update

---
+++ shipment_management/config/app_config.py
@@ -0,0 +1,29 @@
+
+class SupportedProviderList(object):
+    Fedex = 'FEDEX'
+
+
+PRIMARY_FEDEX_DOC_NAME = "Fedex Test Server Config"
+
+
+class FedexTestServerConfiguration(object):
+    key = '0uSKxCgw6AZANfZ5'
+    password = 'WFDeuKsHwGuplTgd7ESLK0FpB'
+    account_number = '510087283'
+    meter_number = '118747441'
+    freight_account_number = '510087020'
+    use_test_server = True
+
+
+EMAIL_SIGNATURE = """
+SincerelyYours,
+Jerry Harvey AudioJHA Team
+__________________________________
+
+This message is auto-generated. Please do not reply to it directly.
+If you have any questions or issues, please contact us by phone (866) 485-9111, fax (407) 814-0018 or drop us a note
+"""
+
+SHORT_COMPANY_NAME = """JH Audio"""
+
+
