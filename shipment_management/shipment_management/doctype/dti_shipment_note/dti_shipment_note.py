f71921efac0bd021a77924c68f4e10a67552131d
KaterinaR <romanchuk.katerina@gmail.com>
2016-11-25 13:10:42 +0200

    Update

--- shipment_management/shipment_management/doctype/dti_shipment_note/dti_shipment_note.py
+++ shipment_management/shipment_management/doctype/dti_shipment_note/dti_shipment_note.py
@@ -13,6 +13,7 @@ from frappe.utils.file_manager import *
 
 from shipment_management.provider_fedex import create_fedex_shipment
 from shipment_management.shipment import ShipmentNoteOperationalStatus
+from shipment_management.shipment import delete_fedex_shipment
 
 fedex_track_service = frappe.get_module("fedex.services.track_service")
 fedex_config = frappe.get_module("fedex.config")
@@ -33,3 +34,12 @@ class DTIShipmentNote(Document):
 			create_fedex_shipment(self)
 			frappe.db.set(self, 'shipment_note_status', ShipmentNoteOperationalStatus.InProgress)
 			frappe.db.set(self, 'fedex_status', ShipmentNoteOperationalStatus.InProgress)
+
+	def on_cancel(self):
+		if self.shipment_provider == SupportedProviderList.Fedex:
+			#delete_fedex_shipment(self)
+
+			frappe.msgprint(_("Shipment {} has been canceled!".format(self.name)))
+
+			frappe.db.set(self, 'shipment_note_status', ShipmentNoteOperationalStatus.Cancelled)
+			frappe.db.set(self, 'fedex_status', ShipmentNoteOperationalStatus.Cancelled)
