f32f6d702ec0edf82d3662bbb2eecef7af7b4a63
KaterinaR <romanchuk.katerina@gmail.com>
2016-12-22 00:16:07 +0200

    Update

--- shipment_management/shipment_management/doctype/dti_shipment_package/dti_shipment_package.js
+++ shipment_management/shipment_management/doctype/dti_shipment_package/dti_shipment_package.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2016, DigiThinkit Inc. and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('DTI Shipment Package', {
-	refresh: function(frm) {}
-});
+//frappe.ui.form.on('DTI Shipment Package', {
+//	refresh: function(frm) {}
+//});
