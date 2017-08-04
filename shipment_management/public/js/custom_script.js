4e92e06c4133bd8b3ab1065a15a210fb5ca44c05
Valmik Jangla <valmik2k2@gmail.com>
2017-08-04 21:16:10 +0530

    [fix] allow shipment notes to be made for pickup and hasownshipping

--- shipment_management/public/js/custom_script.js
+++ shipment_management/public/js/custom_script.js
@@ -1,8 +1,6 @@
 frappe.ui.form.on("Delivery Note", {
     refresh: function (frm) {
-        if (cur_frm.doc.status == "Completed" 
-                && cur_frm.doc.fedex_shipping_method != "PICK UP"
-                && cur_frm.doc.fedex_shipping_method != "SHIP USING MY ACCOUNT") {
+        if (cur_frm.doc.status == "Completed") {
             cur_frm.add_custom_button(__('Shipment'),
                 function () {
                     frappe.call({
