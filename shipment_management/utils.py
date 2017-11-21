eb026c69ce599b84fa6bdc8cb3e10672698630a1
Rohan <rohan@digithinkit.com>
2017-11-21 18:46:12 +0530

    Replace GMaps with PyCountry + removed Shipment Manager settings

--- shipment_management/utils.py
+++ shipment_management/utils.py
@@ -1,47 +1,42 @@
-import frappe
-import requests
 import json
 from collections import defaultdict
+
+import pycountry
+
+import frappe
 from frappe import _
+
 
 def get_state_code(address):
 	if not address.get("state"):
 		return
 
-	if len(address.get("state")) == 2:
-		return address.get("state")
+	try:
+		state = pycountry.subdivisions.lookup(address.get("state"))
+	except LookupError:
+		if len(address.get("state")) == 2:
+			country = pycountry.countries.lookup(address.get("country"))
 
-	URL = "https://maps.googleapis.com/maps/api/geocode/json"
-	params = {
-		"address" : " ".join((address.get("pincode", ""),
-		address.get("city",
-			address.get("state", "")),
-				address.get("country", "")))
-	}
+			if country:
+				state_iso = "{}-{}".format(str(country.alpha_2), address.get("state"))
 
-	google_api_key = frappe.db.get_single_value("Shipment Manager Settings", "google_api_key")
-	if google_api_key:
-		params["key"] = google_api_key
+				try:
+					state = pycountry.subdivisions.lookup(state_iso)
+				except LookupError:
+					error_message = """{} is not a valid state!
+									Check for typos or enter the
+									2-letter ISO code for your state."""
 
-	r = requests.get(URL, params)
+					frappe.throw(_(error_message.format(address.get("state"))))
+					return
+				else:
+					return state.code.split('-')[1]
+	else:
+		return state.code.split('-')[1]
 
-	if r.status_code != 200:
-		frappe.throw(_("Error connecting to Google Maps API"))
-
-	address_data = r.json().get("results")
-	if address_data:
-		address_data = address_data[0].get("address_components")
-	for address_component in address_data:
-		if address_component.get("long_name").lower() == address.get("state").lower():
-			# To make sure that state code is either 2 or 3 letters only.
-			# Google API sometimes sends bad data
-			if address_component.get("short_name"):
-				if len(address_component.get("short_name")) <= 3:
-					return address_component.get("short_name")
 
 def get_country_code(country):
 	return frappe.get_value("Country", country, "code")
-
 
 
 @frappe.whitelist()
@@ -61,8 +56,7 @@ def create_shipment_note(items, item_dict, doc):
 	shipment_doc = frappe.new_doc("DTI Shipment Note")
 	shipment_doc.delivery_note = doc.get("name")
 	for box, items in box_items.items():
-		box_list.append({"physical_packaging" : "BOX",
-			"items_in_box": "\n".join(items)})
+		box_list.append({"physical_packaging": "BOX", "items_in_box": "\n".join(items)})
 
 	shipment_doc.extend("box_list", list(reversed(box_list)))
 
@@ -91,20 +85,21 @@ def create_shipment_note(items, item_dict, doc):
 				item['custom_value'] = item.get("rate")
 			shipment_doc.append("delivery_items", item)
 
-
 	shipment_doc.save()
 	frappe.db.commit()
 
 	return shipment_doc.name
+
 
 @frappe.whitelist()
 def get_stock_items(items):
 	items = json.loads(items)
 	stock_items = []
 	for item in items:
-		if frappe.db.get_value("Item", {"item_code" : item.get("item_code")}, "is_stock_item"):
+		if frappe.db.get_value("Item", {"item_code": item.get("item_code")}, "is_stock_item"):
 			stock_items.append(item)
 	return stock_items
+
 
 @frappe.whitelist()
 def get_packages_in_order(doctype, txt, searchfield, start, page_len, filters):
