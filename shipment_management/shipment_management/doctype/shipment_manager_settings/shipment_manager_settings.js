59c11ce7f615bd3ab296ced900b5f21b6ec37cfc
Alchez <Alchez@users.noreply.github.com>
2017-12-04 12:55:59 +0530

    [WIP] Replace GMaps with PyCountry + removed Shipment Manager settings (#72)
    * Replace GMaps with PyCountry + removed Shipment Manager settings
    
    * Reduce complexity on try-except

--- shipment_management/shipment_management/doctype/shipment_manager_settings/shipment_manager_settings.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2017, DigiThinkit Inc. and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Shipment Manager Settings', {
-	refresh: function(frm) {
-
-	}
-});
