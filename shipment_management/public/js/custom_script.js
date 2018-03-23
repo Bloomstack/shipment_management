a712698bd9584f1fa80b5962902c755eb5fc8e6b
Rohan <rohan@digithinkit.com>
2018-03-23 18:33:22 +0530

    Allow creation of Shipment Notes if the sales order is marked as 'Authorize Delivery'

--- shipment_management/public/js/custom_script.js
+++ shipment_management/public/js/custom_script.js
@@ -1,6 +1,18 @@
 frappe.ui.form.on("Delivery Note", {
     refresh: function (frm) {
-        if (cur_frm.doc.status == "Completed" && cur_frm.fedex_shipping_method != "PICK UP") {
+        var authorize_status = null
+
+        for (i in cur_frm.doc.items) {
+            var item = cur_frm.doc.items[i];
+
+            if (!authorize_status) {
+                authorize_status = parseInt(frappe.model.get_value("Sales Order", item.against_sales_order, "authorize_delivery"));
+            } else {
+                break;
+            }
+        }
+
+        if (authorize_status && cur_frm.doc.fedex_shipping_method != "PICK UP") {
             cur_frm.add_custom_button(__('Shipment'),
                 function () {
                     frappe.call({
