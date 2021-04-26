efcd4599aed692ff1605a29b25a2e9586cb08ae0
vishdha <vishaldhayagude07@gmail.com>
2021-04-26 16:48:36 +0530

    fix: list index out of range issue resolved

--- shipment_management/shipment_management/doctype/dti_shipment_note/dti_shipment_note.py
+++ shipment_management/shipment_management/doctype/dti_shipment_note/dti_shipment_note.py
@@ -55,7 +55,8 @@ class DTIShipmentNote(Document):
 		self.set_tracking_ids()
 
 		frappe.db.set(self, 'fedex_status', ShipmentNoteOperationalStatus.InProgress)
-		frappe.db.set(self, 'tracking_number', self.box_list[0].tracking_number)
+		if self.box_list:
+			frappe.db.set(self, 'tracking_number', self.box_list[0].tracking_number)
 
 	# def on_cancel(self):
 		# from shipment_management.config.app_config import SupportedProviderList
